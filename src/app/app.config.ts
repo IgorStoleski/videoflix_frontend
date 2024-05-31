f19c8db6e9dd31d024065f1c30f0f8b70d90d739
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-05-31 09:04:51 +0200

    add material.module.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MaterialModule } from './shared/modules/material.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    MaterialModule, provideAnimationsAsync()
+  ]
 };
