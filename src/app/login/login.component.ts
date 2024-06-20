99505646aa3386f3afaa36ea3285879323652aaa
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-06-20 21:52:10 +0200

    add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,73 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { MaterialModule } from '../shared/module/material/material.module';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from './../auth/auth.service';
+import {merge} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    MaterialModule,
+    ReactiveFormsModule,
     RouterModule,
-    MaterialModule
+    RouterLink
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  error: string | null = null;
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
 
+  errorMessage = signal('');
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  constructor(private authService: AuthService, private router: Router) {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  protected loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  onSubmit() {
+    console.log("Form Validity:", this.loginForm.valid);
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value)
+        .subscribe({
+          next: () => {
+            if (this.authService.isLoggedIn()) {
+              this.router.navigate(['/dashboard/']);
+            }
+          },
+          error: () => {
+            this.error = 'Username or password is invalid'; 
+          }
+        });
+    }
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  
 }
